0f78bee5f56ae01d03ac40ce135d28c56c31f1d6
polymonster <alexandercdixon@gmail.com>
2017-03-24 14:58:52 +0000

    get build textures python script running in a platfrom agnostic manner, texture compression tools working now on osx

--- tools/build_textures.py
+++ tools/build_textures.py
@@ -1,15 +1,19 @@
-
 import os
 import subprocess
+import shutil
 
-#win32 / dds / block compression / mips / cubemaps
+# win32 / dds / block compression / mips / cubemaps
 print("texture conversion, mip generation and compression" + "\n")
 
-nvtt_dir = os.getcwd() + "\\..\\tools\\bin\\nvtt\\win32\\"
-texture_dir = os.getcwd() + "\\assets\\textures\\"
-build_dir = os.getcwd() + "\\bin\\win32\\data\\textures\\"
+platform_name = "win32"
+if os.name == "posix":
+    platform_name = "osx"
 
-#create textures dir
+nvtt_dir = os.path.join(os.getcwd(), "..", "tools", "bin", "nvtt", platform_name, "nvcompress")
+texture_dir = os.path.join(os.getcwd(), "assets", "textures")
+build_dir = os.path.join(os.getcwd(), "bin", platform_name, "data", "textures")
+
+# create textures dir
 if not os.path.exists(build_dir):
     os.makedirs(build_dir)
 
@@ -18,13 +22,16 @@ print("processing directory: " + texture_dir + "\n")
 for f in os.listdir(texture_dir):
     [fnoext, fext] = os.path.splitext(f)
     print(f)
-    print(fnoext)
-    print(nvtt_dir)
+    dds_filename = fnoext + ".dds"
+    dest_path = os.path.join(build_dir, dds_filename)
+    src_path = os.path.join(texture_dir, f)
     if f.find(".png") != -1 or f.find(".jpg") or f.find(".tif") != -1:
-        print("converting texture")
-        cmdline = nvtt_dir + "nvcompress -rgb " + texture_dir + f + " " + build_dir + fnoext + ".dds"
+        print("converting texture to data dir")
+        cmdline = nvtt_dir + " -rgb " + src_path + " " + dest_path
+        subprocess.check_call(cmdline, shell=True)
     if f.find(".dds") != -1:
-        print("copying texture")
-        #straight copy
-    subprocess.check_call(cmdline)
+        print("copying texture to data dir")
+        # straight copy
+        dest_file = os.path.join(build_dir, f)
+        shutil(f, dest_file)
     print("\n")
