78133b1af1a7c8e5dd274d3b7f7794ca0ff2be79
polymonster <alexandercdixon@gmail.com>
2017-11-07 17:29:17 +0000

    fixing up shaders for cross platform 3x3 casts

--- tools/_shader_macros.h
+++ tools/_shader_macros.h
@@ -5,10 +5,14 @@
 #define mul( A, B ) A * B
 #define saturate( A ) clamp( A, 0.0, 1.0 );
 
+#define to_3x3( M4 ) float3x3(M4)
+
 #else
 	
 #define TEXTURE_2D( name, sampler_index ) Texture2D name : register(t##sampler_index); ; SamplerState sampler_##name : register(s##sampler_index); 
 #define SAMPLE_TEXTURE_2D( name, uv ) name.Sample( sampler_##name, uv )
+
+#define to_3x3( M4 ) (float3x3)M4
 
 #endif
 
