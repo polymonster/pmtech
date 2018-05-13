d8deac8556194407a083583644598403756835a4
polymonster <alexandercdixon@gmail.com>
2018-05-13 14:48:19 +0100

    add timer linux

--- pen/source/linux/os.cpp
+++ pen/source/linux/os.cpp
@@ -16,13 +16,8 @@ int main (int argc, char *argv[])
     Display                 *display;
     Visual                  *visual;
     int                     depth;
-    int                     text_x;
-    int                     text_y;
     XSetWindowAttributes    frame_attributes;
     Window                  frame_window;
-    XFontStruct             *fontinfo;
-    XGCValues               gr_values;
-    GC                      graphical_context;
     XEvent               	event;
 
     display = XOpenDisplay(NULL);
@@ -30,52 +25,19 @@ int main (int argc, char *argv[])
     depth  = DefaultDepth(display, 0);
     
     frame_window = XCreateWindow(display, XRootWindow(display, 0),
-                                 0, 0, 400, 400, 5, depth,
+                                 0, 0, pen_window.width, pen_window.height, 5, depth,
                                  InputOutput, visual, CWBackPixel,
                                  &frame_attributes);
 
     XStoreName(display, frame_window, pen_window.window_title);
     XSelectInput(display, frame_window, ExposureMask | StructureNotifyMask);
 
-    //fontinfo = XLoadQueryFont(display, "10x20");
-    //gr_values.font = fontinfo->fid;
-    //gr_values.foreground = XBlackPixel(display, 0);
-    //graphical_context = XCreateGC(display, frame_window, 
-    //                              GCFont+GCForeground, &gr_values);
-
     XMapWindow(display, frame_window);
 
     while ( 1 ) 
 	{
 		XNextEvent(display, (XEvent *)&event);
-
-		#if 0
-        
-        switch ( event.type ) {
-            case Expose:
-            {
-                XWindowAttributes windnamespace pen
-{
-
-}ow_attributes;
-                int font_direction, font_ascent, font_descent;
-                XCharStruct text_structure;
-                XTextExtents(fontinfo, hello_string, hello_string_length, 
-                             &font_direction, &font_ascent, &font_descent, 
-                             &text_structure);
-                XGetWindowAttributes(display, frame_window, &window_attributes);
-                text_x = (window_attributes.width - text_structure.width)/2;
-                text_y = (window_attributes.height - 
-                          (text_structure.ascent+text_structure.descent))/2;
-                XDrawString(display, frame_window, graphical_context,
-                            text_x, text_y, hello_string, hello_string_length);
-                break;
-            }
-            default:
-                break;
-        }
-		#endif
-
     }
+	
     return(0);
 }
---
+++ pen/source/linux/timer.cpp
@@ -0,0 +1,102 @@
+#include "timer.h"
+#include "pen_string.h"
+#include <mach/mach.h>
+#include <mach/mach_time.h>
+#include <unistd.h>
+
+#include "console.h"
+
+namespace pen
+{
+#define MAX_TIMERS 100
+	typedef struct timer
+	{
+        uint64_t last_start;
+
+		f32 accumulated;
+		f32 longest;
+		f32 shortest;
+
+		u32	hit_count;
+
+		const c8* name;
+
+	} timer;
+
+	//LARGE_INTEGER permonace_frequency;
+	f32			  ticks_to_ms;
+    f32			  ticks_to_us;
+    f32			  ticks_to_ns;
+    
+    //fixed array.. todo sort this out
+	timer		  timers[ MAX_TIMERS ];
+
+	u32 next_free = 0;
+
+	void timer_system_intialise( )
+	{
+        static mach_timebase_info_data_t s_timebase_info;
+        
+        if ( s_timebase_info.denom == 0 )
+        {
+            (void) mach_timebase_info(&s_timebase_info);
+        }
+        
+        ticks_to_ns = (f32)s_timebase_info.numer / (f32)s_timebase_info.denom;
+        ticks_to_us = ticks_to_ns / 1000.0f;
+        ticks_to_ms = ticks_to_us / 1000.0f;
+
+		next_free = 0;
+	}
+
+	u32 timer_create( const c8* name )
+	{
+		timers[ next_free ].name = name;
+		PEN_ASSERT( next_free < MAX_TIMERS );
+		return next_free++;
+	}
+
+	void timer_start( u32 index )
+	{
+        timers[ index ].last_start = mach_absolute_time();
+	}
+    
+	f32 timer_elapsed_ms( u32 timer_index )
+	{
+        uint64_t t = mach_absolute_time() - timers[ timer_index ].last_start;
+		return t * ticks_to_ms;
+	}
+    
+    f32 timer_elapsed_us( u32 timer_index )
+    {
+        uint64_t t = mach_absolute_time() - timers[ timer_index ].last_start;
+        return t * ticks_to_us;
+    }
+    
+    f32 timer_elapsed_ns( u32 timer_index )
+    {
+        uint64_t t = mach_absolute_time() - timers[ timer_index ].last_start;
+        return t * ticks_to_ns;
+    }
+
+	f32 get_time_ms( )
+	{
+        uint64_t t = mach_absolute_time();
+        
+        return (f32)( t ) * ticks_to_ms;
+	}
+    
+    f32 get_time_us( )
+    {
+        uint64_t t = mach_absolute_time();
+        
+        return (f32)( t ) * ticks_to_us;
+    }
+    
+    f32 get_time_ns( )
+    {
+        uint64_t t = mach_absolute_time();
+        
+        return (f32)( t ) * ticks_to_ns;
+    }
+}
