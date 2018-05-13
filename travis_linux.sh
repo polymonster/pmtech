bf0cf67c2a8832ae856641b3fd7ee62001dc9d5b
Alex Dixon <alexandercdixon@gmail.com>
2018-05-13 20:52:05 +0100

    Update travis_linux.sh

--- travis_linux.sh
+++ travis_linux.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+cd put/bullet
+../../tools/premake/premake5_linux gmake
+cd build
+make config=debug
+make config=release
 cd examples
 python3 ../tools/build.py -actions code -ide gmake -platform linux
 cd build/linux
