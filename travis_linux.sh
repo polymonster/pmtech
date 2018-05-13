b890a75a34b1c573d040e041f2238007ac21d085
polymonster <alexandercdixon@gmail.com>
2018-05-13 21:12:36 +0100

    full build script +bullet

--- travis_linux.sh
+++ travis_linux.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 cd put/bullet
+ls
 ../../tools/premake/premake5_linux gmake
-cd build
+cd build/linux
 make config=debug
 make config=release
+cd ../../../../
 cd examples
 python3 ../tools/build.py -actions code -ide gmake -platform linux
 cd build/linux
