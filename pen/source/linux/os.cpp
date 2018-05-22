96983a28f35b7bcbcde0ed157cf511d3ef3b76f7
polymonster <alexandercdixon@gmail.com>
2018-05-22 19:57:25 +0100

    linux input

--- pen/source/linux/os.cpp
+++ pen/source/linux/os.cpp
@@ -130,7 +130,7 @@ int main(int argc, char* argv[])
     glXCreateContextAttribsARB =
         (glXCreateContextAttribsARBProc)glXGetProcAddressARB((const GLubyte*)"glXCreateContextAttribsARB");
 
-    int context_attribs[] = {GLX_CONTEXT_MAJOR_VERSION_ARB, 3, GLX_CONTEXT_MINOR_VERSION_ARB, 0, None };
+    int context_attribs[] = {GLX_CONTEXT_MAJOR_VERSION_ARB, 3, GLX_CONTEXT_MINOR_VERSION_ARB, 1, None };
 
     _gl_context = glXCreateContextAttribsARB(_display, best_fbc, 0, True, context_attribs);
 
@@ -175,6 +175,64 @@ namespace pen
         return filename;
     }
 
+    s32 translate_mouse_button(s32 b)
+    {
+        switch(b)
+        {
+            case 1:
+            return 0;
+            case 3:
+            return 1;
+            case 2:
+            return 2;
+        };
+
+        return -1;
+    }
+
+    u32 translate_key_sym(u32 k)
+    {
+        if(k >= XK_KP_Space && k <= XK_KP_9)
+            return k;
+
+        switch(k)
+        {
+            case XK_BackSpace: return PK_BACK;
+            case XK_Tab: return PK_TAB;
+            case XK_Linefeed: return 0; //?
+            case XK_Clear: return PK_CLEAR;
+            case XK_Return: return PK_RETURN;
+            case XK_Pause: return PK_PAUSE;
+            case XK_Scroll_Lock: return PK_SCROLL;
+            case XK_Sys_Req: return 0; //?
+            case XK_Escape: return PK_ESCAPE;
+            case XK_Delete: return PK_DELETE;
+            case XK_Home: return PK_HOME;
+            case XK_Left: return PK_LEFT;
+            case XK_Up: return PK_UP;
+            case XK_Right: return PK_RIGHT;
+            case XK_Down: return PK_DOWN;
+            case XK_Prior: return PK_PRIOR;
+            case XK_Next: return PK_NEXT;
+
+            case XK_F1: return PK_F1;
+            case XK_F2: return PK_F2;
+            case XK_F3: return PK_F3;
+            case XK_F4: return PK_F4;
+            case XK_F5: return PK_F5;
+            case XK_F6: return PK_F6;
+            case XK_F7: return PK_F7;
+            case XK_F8: return PK_F8;
+            case XK_F9: return PK_F9;
+            case XK_F10: return PK_F10;
+            
+            case XK_F11: return PK_F11;
+            case XK_F12: return PK_F12;
+        };
+
+        return 0;
+    }
+
     bool os_update()
     {
         static bool init_jobs = false;
@@ -193,50 +251,73 @@ namespace pen
             XNextEvent(_display, (XEvent*)&event);
             switch (event.type)
             {
-            case Expose:
-            {
-                XWindowAttributes attribs;
-                XGetWindowAttributes(_display, _window, &attribs);
-                pen_window.width  = attribs.width;
-                pen_window.height = attribs.height;
-            }
-            break;
-            case KeyPress:
-            {
-                static c8 buf[255];
-                KeySym    k;
-                XLookupString(&event.xkey, buf, 255, &k, 0);
-                pen::input_set_unicode_key_down(buf[0]);
-                pen::input_set_key_down(event.xkey.keycode);
-            }
-            break;
-            case KeyRelease:
-            {
-                static c8 buf[255];
-                KeySym    k;
-                XLookupString(&event.xkey, buf, 255, &k, 0);
-                pen::input_set_unicode_key_up(buf[0]);
-                pen::input_set_key_up(event.xkey.keycode);
-            }
-            break;
-            case ButtonPress:
-            {
-                // event.xbutton.x, event.xbutton.y
-            }
-            break;
-            case MotionNotify:
-            {
-                pen::input_set_mouse_pos(event.xmotion.x, event.xmotion.y);
-            }
-            break;
-            }
+                case Expose:
+                {
+                    XWindowAttributes attribs;
+                    XGetWindowAttributes(_display, _window, &attribs);
+                    pen_window.width  = attribs.width;
+                    pen_window.height = attribs.height;
+                }
+                break;
+                case KeyPress:
+                {
+                    static c8 buf[255];
+                    KeySym    k;
 
-            static bool pen_terminate_app = false;
-            if(pen_terminate_app)
-            {
-                if(pen::thread_terminate_jobs())
-                    return false;
+                    // todo for utf8
+                    //Xutf8LookupString(xic, &event.xkey, buf, sizeof(buf) - 1, &keysym, &status);
+
+                    XLookupString(&event.xkey, buf, 255, &k, 0);
+                    pen::input_set_unicode_key_down(buf[0]);
+
+                    u32 ks = translate_key_sym(k);
+                    pen::input_set_key_down(ks);
+                }
+                break;
+                case KeyRelease:
+                {
+                    static c8 buf[255];
+                    KeySym    k;
+
+                    XLookupString(&event.xkey, buf, 255, &k, 0);
+                    pen::input_set_unicode_key_up(buf[0]);
+
+                    u32 ks = translate_key_sym(k);
+                    pen::input_set_key_up(ks);
+                }
+                break;
+                case ButtonPress:
+                {
+                    s32 mb = translate_mouse_button(event.xbutton.button);
+                    if(mb != -1)
+                        pen::input_set_mouse_down(mb);
+                }
+                break;
+                case ButtonRelease:
+                {
+                    s32 mb = translate_mouse_button(event.xbutton.button);
+                    if(mb != -1)
+                        pen::input_set_mouse_up(mb);
+                }
+                break;
             }
+        }
+
+        // Update cursor
+        int root_x, root_y;
+        int win_x, win_y;
+        unsigned int mask_return;
+        Window window_returned;
+
+        int result = XQueryPointer(_display, _window, &window_returned, &window_returned, &root_x, &root_y, &win_x, &win_y, &mask_return);
+        pen::input_set_mouse_pos((f32)win_x, (f32)win_y);
+
+        // Check for terminate and poll terminated jobs
+        static bool pen_terminate_app = false;
+        if(pen_terminate_app)
+        {
+            if(pen::thread_terminate_jobs())
+                return false;
         }
 
         return true;
