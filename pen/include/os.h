0decb386c6a4958d404d9ad493c6ceddaa957049
polymonster <alexandercdixon@gmail.com>
2018-05-23 19:31:18 +0100

    more options and ui for volume generation, compile fix for win32

--- pen/include/os.h
+++ pen/include/os.h
@@ -16,7 +16,7 @@ namespace pen
     bool os_update();
     void os_set_cursor_pos(u32 client_x, u32 client_y);
     void os_show_cursor(bool show);
-    const c8* os_path_for_resource( const c8* filename );
+    const c8* os_path_for_resource(const c8* filename);
     
 } // namespace pen
 
