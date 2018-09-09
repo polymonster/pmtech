220e5d3caf9a0f0b5ad9e03d7b72c315c71fec36
polymonster <alexandercdixon@gmail.com>
2018-09-09 17:50:52 +0100

    bash script for linux or osx

--- third_party/build_libs.sh
+++ third_party/build_libs.sh
@@ -4,7 +4,12 @@
 
 # bullet
 cd bullet
-../../tools/premake/premake5 gmake --platform_dir=$1
+if [ "$1" = "linux" ]; then
+    ../../tools/premake/premake5_linux gmake --platform_dir=$1
+else
+    ../../tools/premake/premake5 gmake --platform_dir=$1
+fi
+
 cd build/$1
 make config=debug
 make config=release
