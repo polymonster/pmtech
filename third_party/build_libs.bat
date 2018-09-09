c0e6de42f5793f52c197c5dcccca8b9f1397ec15
polymonster <alexandercdixon@gmail.com>
2018-09-09 19:17:41 +0100

    bullet build

--- third_party/build_libs.bat
+++ third_party/build_libs.bat
@@ -3,7 +3,7 @@
 :: bullet
 cd bullet
 ..\..\tools\premake\premake5.exe vs2017 --platform_dir="win32"
-cd build/win32
+cd build/vs2017
 msbuild bullet_monolithic.sln /p:Configuration=Debug
 msbuild bullet_monolithic.sln /p:Configuration=Release
 cd ..\..\..
