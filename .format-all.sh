b1fcac58dfe3ac277b5c69286ed043faa7fdc152
polymonster <alexandercdixon@gmail.com>
2018-10-29 23:16:40 +0000

    update maths submodule

--- .format-all.sh
+++ .format-all.sh
@@ -4,7 +4,8 @@ find pen/include -name '**.inl' -or -name '*.h' | xargs clang-format -style=file
 find pen/source -name '**.cpp' -or -name '*.h' -or -name '*mm' -or -name '.c' | xargs clang-format -style=file -i
 find put/include -name '**.inl' -or -name '*.h' | xargs clang-format -style=file -i
 find put/source -name '**.cpp' -or -name '*.h' | xargs clang-format -style=file -i
-find assets -name '*.jsn' -or -name '*.pmfx' | xargs python3 tools/build_scripts/stub_format.py -tabs_to_spaces 4 -align_consecutive : -i 
+find assets -name '*.jsn' -or -name '*.pmfx' | xargs python3 tools/build_scripts/stub_format.py -tabs_to_spaces 4 -align_consecutive : -i
+find examples/assets -name '*.jsn' -or -name '*.pmfx' | xargs python3 tools/build_scripts/stub_format.py -tabs_to_spaces 4 -align_consecutive : -i 
 git add .
 git commit -m "format-all"
 git push
