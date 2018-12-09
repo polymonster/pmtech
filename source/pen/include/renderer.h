a26041e845b64dde3610b377c20eb122d927be2c
polymonster <alexandercdixon@gmail.com>
2018-12-09 11:51:30 +0000

    changing set texture api

--- source/pen/include/renderer.h
+++ source/pen/include/renderer.h
@@ -290,7 +290,9 @@ namespace pen
     enum e_texture_bind_flags
     {
         TEXTURE_BIND_NO_FLAGS = 0,
-        TEXTURE_BIND_MSAA = 1
+        TEXTURE_BIND_MSAA = 1,
+        TEXTURE_BIND_PS = 1<<1,
+        TEXTURE_BIND_VS = 1<<2
     };
 
     enum e_msaa_resolve_type
