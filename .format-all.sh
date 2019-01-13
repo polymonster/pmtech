a7857a0745ae6c20d5a9dc53d706e6034d780f7b
polymonster <alexandercdixon@gmail.com>
2019-01-13 11:41:52 +0000

    format-all

--- .format-all.sh
+++ .format-all.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-find examples/code -name '**.cpp' -or -name '**.h' | xargs clang-format -style=file -i
-find source -name '**.inl' -or -name '**.h' | xargs clang-format -style=file -i
-find source -name '**.cpp' -or -name '**.h' -or -name '*.mm' -or -name '*.c' | xargs clang-format -style=file -i
-find assets -name '*.jsn' -or -name '*.pmfx' | xargs python3 tools/build_scripts/stub_format.py -tabs_to_spaces 4 -w -i
+find examples/code -name '**.cpp' -or -name '**.h' | xargs clang-format -style=file -i 
+find source -name '**.inl' -or -name '**.h' | xargs clang-format -style=file -i 
+find source -name '**.cpp' -or -name '*.mm' -or -name '*.c' | xargs clang-format -style=file -i 
+find assets -name '*.jsn' -or -name '*.pmfx' | xargs python3 tools/build_scripts/stub_format.py -tabs_to_spaces 4 -w -i 
 find examples/assets -name '*.jsn' -or -name '*.pmfx' | xargs python3 tools/build_scripts/stub_format.py -tabs_to_spaces 4 -w -i 
 find source -name '*.lua'| xargs python3 tools/build_scripts/stub_format.py -tabs_to_spaces 4 -w -i 
 git add .
