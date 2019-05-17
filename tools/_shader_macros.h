85dbc447e159e92b228c50f5930519da126214ca
polymonster <alexandercdixon@gmail.com>
2019-05-17 20:43:47 +0100

    - shader macros

--- tools/_shader_macros.h
+++ tools/_shader_macros.h
@@ -128,7 +128,7 @@
     #define _pmfx_unroll
 #endif //METAL
 
-// generic
+//GENERIC_MACROS
 #define chebyshev_normalize( V ) (V.xyz / max( max(abs(V.x), abs(V.y)), abs(V.z) ))	
 #define max3(v) max(max(v.x, v.y),v.z)
 #define max4(v) max(max(max(v.x, v.y),v.z), v.w)
