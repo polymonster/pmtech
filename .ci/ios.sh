481402fb024857785abad21b2f4c7309bc8dd7ce
polymonster <alexandercdixon@gmail.com>
2020-03-24 20:55:19 +0000

    - add correct schemes to ios build, disable code signing

--- .ci/ios.sh
+++ .ci/ios.sh
@@ -2,5 +2,5 @@
 cd examples
 ../pmbuild ios-ci -libs -premake
 cd build/ios
-xcodebuild -scheme examples -destination generic/platform=iOS -workspace pmtech_examples_ios.xcworkspace -configuration Debug
-xcodebuild -scheme examples -destination generic/platform=iOS -workspace pmtech_examples_ios.xcworkspace -configuration Release
+xcodebuild -scheme pen -destination generic/platform=iOS -workspace pmtech_examples_ios.xcworkspace -configuration Debug CODE_SIGNING_ALLOWED=NO
+xcodebuild -scheme put -destination generic/platform=iOS -workspace pmtech_examples_ios.xcworkspace -configuration Debug CODE_SIGNING_ALLOWED=NO
