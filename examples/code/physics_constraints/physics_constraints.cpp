a8499347975696bae519d4a3d6572d236de01da7
polymonster <alexandercdixon@gmail.com>
2020-09-06 16:21:43 +0100

    - fix typo in physics contrainst sample, and associated output metadata

--- examples/code/physics_constraints/physics_constraints.cpp
+++ examples/code/physics_constraints/physics_constraints.cpp
@@ -10,7 +10,7 @@ namespace pen
         pen::pen_creation_params p;
         p.window_width = 1280;
         p.window_height = 720;
-        p.window_title = "physics_constarints";
+        p.window_title = "physics_constraints";
         p.window_sample_count = 4;
         p.user_thread_function = user_setup;
         p.flags = pen::e_pen_create_flags::renderer;
