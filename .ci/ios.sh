e2de300bfe7aedeb8b0d06f2e9fb7dbb24e5606f
polymonster <alexandercdixon@gmail.com>
2020-11-21 21:22:48 +0000

    - fix linux and ios

--- .ci/ios.sh
+++ .ci/ios.sh
@@ -5,4 +5,3 @@ cd examples
 ../pmbuild ios
 ../pmbuild make ios all -destination generic/platform=iOS -configuration Debug CODE_SIGNING_ALLOWED=NO -quiet
 ../pmbuild make ios all -destination generic/platform=iOS -configuration Release CODE_SIGNING_ALLOWED=NO -quiet
-
