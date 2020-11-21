3d8ba1cb2fc240871b6bbe532a283bd797644bb0
polymonster <alexandercdixon@gmail.com>
2020-11-21 15:36:11 +0000

    -ci

--- .ci/ios.sh
+++ .ci/ios.sh
@@ -3,6 +3,6 @@ python3 -m pip install cryptography
 cd examples
 ../pmbuild ios -libs
 ../pmbuild ios
-pmbuild make ios all -destination generic/platform=iOS -configuration Debug CODE_SIGNING_ALLOWED=NO
-pmbuild make ios all -destination generic/platform=iOS -configuration Release CODE_SIGNING_ALLOWED=NO
+pmbuild make ios all -destination generic/platform=iOS -configuration Debug CODE_SIGNING_ALLOWED=NO -quiet
+pmbuild make ios all -destination generic/platform=iOS -configuration Release CODE_SIGNING_ALLOWED=NO -quiet
 
