a114b8553854648d19c2f1ccf9c47fa33a8bd2ba
polymonster <alexandercdixon@gmail.com>
2020-11-21 15:04:31 +0000

    - ci|

--- .ci/mac-metal.sh
+++ .ci/mac-metal.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 python3 -m pip install cryptography
 cd examples
-../pmbuild mac
 ../pmbuild mac -libs
-ls
 ../pmbuild mac
 ../pmbuild mac make all -configuration Debug
 ../pmbuild mac make all -configuration Release
