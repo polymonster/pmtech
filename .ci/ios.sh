7398789acecde96c33b43716d1dc4b231b897938
polymonster <alexandercdixon@gmail.com>
2020-11-23 21:55:33 +0000

    - fix ios ci

--- .ci/ios.sh
+++ .ci/ios.sh
@@ -2,4 +2,5 @@
 cd examples
 ../pmbuild ios-ci -libs
 ../pmbuild ios-ci
+../pmbuild make ios-ci all -destination generic/platform=iOS -configuration Debug CODE_SIGNING_ALLOWED=NO -quiet
 ../pmbuild make ios-ci all -destination generic/platform=iOS -configuration Release CODE_SIGNING_ALLOWED=NO -quiet
--- .ci/mac-metal.sh
+++ .ci/mac-metal.sh
@@ -2,4 +2,5 @@
 cd examples
 ../pmbuild mac -libs
 ../pmbuild mac
+../pmbuild make mac all -configuration Debug -quiet
 ../pmbuild make mac all -configuration Release -quiet
