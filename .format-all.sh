f34150426233c3d089b4da0623ba1c2ec7362035
polymonster <alexandercdixon@gmail.com>
2020-11-23 23:31:26 +0000

    format-all

--- .format-all.sh
+++ .format-all.sh
@@ -2,9 +2,9 @@
 find examples/code -name '**.cpp' -or -name '**.h' | xargs clang-format -style=file -i 
 find source -name '**.inl' -or -name '**.h' | xargs clang-format -style=file -i 
 find source -name '**.cpp' -or -name '*.mm' -or -name '*.c' | xargs clang-format -style=file -i 
-find assets -name '*.jsn' -or -name '*.pmfx' | xargs python3 tools/scripts/stub_format.py -tabs_to_spaces 4 -w -i
-find examples/assets -name '*.jsn' -or -name '*.pmfx' | xargs python3 tools/scripts/stub_format.py -tabs_to_spaces 4 -w -i
-find source -name '*.lua'| xargs python3 tools/scripts/stub_format.py -tabs_to_spaces 4 -w -i
+find assets -name '*.jsn' -or -name '*.pmfx' | xargs python3 tools/pmbuild_ext/stub_format.py -tabs_to_spaces 4 -w -i
+find examples/assets -name '*.jsn' -or -name '*.pmfx' | xargs python3 tools/pmbuild_ext/stub_format.py -tabs_to_spaces 4 -w -i
+find source -name '*.lua'| xargs python3 tools/pmbuild_ext/stub_format.py -tabs_to_spaces 4 -w -i
 git add .
 git commit -m "format-all"
 git push
