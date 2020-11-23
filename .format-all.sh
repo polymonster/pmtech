8cb677c3fd76a5a2ab10007657bbbc1cffcc1a8a
polymonster <alexandercdixon@gmail.com>
2020-11-23 23:44:14 +0000

    format-all

--- .format-all.sh
+++ .format-all.sh
@@ -3,7 +3,7 @@ find examples/code -name '**.cpp' -or -name '**.h' | xargs clang-format -style=f
 find core -name '**.inl' -or -name '**.h' | xargs clang-format -style=file -i
 find core -name '**.cpp' -or -name '*.mm' -or -name '*.c' | xargs clang-format -style=file -i
 find assets -name '*.jsn' -or -name '*.pmfx' | xargs python3 tools/pmbuild_ext/stub_format.py -tabs_to_spaces 4 -w -i
-find examples/assets -name '*.jsn' -or -name '*.pmfx' | xargs python3 tools/pmbuild_ext/stub_format.py -tabs_to_spaces 4 -w -i
+find examples -name '*.jsn' -or -name '*.pmfx' | xargs python3 tools/pmbuild_ext/stub_format.py -tabs_to_spaces 4 -w -i
 find tools -name '*.lua'| xargs python3 tools/pmbuild_ext/stub_format.py -tabs_to_spaces 4 -w -i
 find core -name '*.lua'| xargs python3 tools/pmbuild_ext/stub_format.py -tabs_to_spaces 4 -w -i
 git add .
