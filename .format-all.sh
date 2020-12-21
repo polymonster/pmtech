ed9f431c2272e88145c02ce6a4eadeac9a7b1d44
polymonster <alexandercdixon@gmail.com>
2020-12-21 17:34:04 +0000

    - formatting/updat pmbuild

--- .format-all.sh
+++ .format-all.sh
@@ -2,10 +2,9 @@
 find examples/code -name '**.cpp' -or -name '**.h' | xargs clang-format -style=file -i 
 find core -name '**.inl' -or -name '**.h' | xargs clang-format -style=file -i
 find core -name '**.cpp' -or -name '*.mm' -or -name '*.c' | xargs clang-format -style=file -i
-find assets -name '**.jsn' -or -name '**.pmfx' | xargs python3 tools/pmbuild_ext/stub_format.py -tabs_to_spaces 4 -w -i
-find examples -name '**.jsn' -or -name '**.pmfx' | xargs python3 tools/pmbuild_ext/stub_format.py -tabs_to_spaces 4 -w -i
-find tools -name '*.lua'| xargs python3 tools/pmbuild_ext/stub_format.py -tabs_to_spaces 4 -w -i
-find core -name '*.lua'| xargs python3 tools/pmbuild_ext/stub_format.py -tabs_to_spaces 4 -w -i
+find . -name '**.jsn' | xargs python3 tools/pmbuild_ext/stub_format.py -tabs 4 -w -i
+find . -name '**.pmfx' | xargs python3 tools/pmbuild_ext/stub_format.py -tabs 4 -w -i
+find . -name '*.lua'| xargs python3 tools/pmbuild_ext/stub_format.py -tabs_to_spaces 4 -w -i
 git add .
 git commit -m "format-all"
 git push
