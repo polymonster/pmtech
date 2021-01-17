e0de7ecd306706a05741cff077d3bb40e74831cc
polymonster <alexandercdixon@gmail.com>
2021-01-17 17:10:26 +0000

    - fix linux ci

--- .ci/linux-gl.sh
+++ .ci/linux-gl.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 cd examples
 ../pmbuild linux -libs
-../pmbuild linux
+../pmbuild linux -help
+../pmbuild linux -all
 ../pmbuild make linux all config=debug
 ../pmbuild make linux all config=release
