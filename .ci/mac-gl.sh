489e826b14cae87fb5a134d44a6f0ea74f1606fb
polymonster <alexandercdixon@gmail.com>
2021-04-06 17:19:44 +0100

    - remove codsigning to fix xcodebuilds

--- .ci/mac-gl.sh
+++ .ci/mac-gl.sh
@@ -2,4 +2,4 @@
 cd examples
 ../pmbuild mac-gl -libs
 ../pmbuild mac-gl
-../pmbuild make mac-gl all -configuration Release -quiet
+../pmbuild make mac-gl all -configuration Release -quiet CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
