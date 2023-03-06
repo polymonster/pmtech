c8a1cb038a573831e356e5d00e27c57380876d69
polymonster <alexandercdixon@gmail.com>
2023-03-06 13:00:47 +0000

    - add glew install step to linux editor

--- .ci/linux-gl.sh
+++ .ci/linux-gl.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 cd examples
 ../pmbuild linux -libs
-../pmbuild linux -help
 ../pmbuild linux -all
 ../pmbuild make linux all config=debug
 ../pmbuild make linux all config=release
